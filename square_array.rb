55e999e9475708d675e5006eb64db6b1948fb28c
Travis Gandy <travis_gandy@yahoo.com>
2020-03-11 04:28:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ def square_array(array)
   array = [4,8,12]
   new_array = []
   array.each do |number| 
+    puts " the square of #{number} is #{number**2}"
     new_array << number**2
  end
   new_array
